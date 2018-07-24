eeaf8f1779315d4060003e0037efd0c5ec99bcd1
William Mitchell <billy.mitchell@levvel.com>
2018-07-24 16:01:50 -0400

    Added component BurgerBuilder to the App.js file and did minimal css work, end of Video 2

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,17 @@
-import React from 'react'; 
+import React from "react";
 
-import Aux from '../../hoc/Aux';
+import Aux from "../../hoc/Aux2";
+import classes from "./Layout.css"
 
-const Layout = (props) => {
-    return (  
-        <Aux>
-        <div> Toolbar, SideDrawer, Backdrop</div>
-        <main>
-            {props.children}
-        </main>
+const Layout = props => {
+  return (
+    <Aux>
+      <div> Toolbar, SideDrawer, Backdrop</div>
+      <main className = {classes.Content}>
+         {props.children}
+      </main>
     </Aux>
+  );
+};
 
-
-    );
-}
- 
 export default Layout;
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Aux2 from "../../hoc/Aux2";
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Aux2>
+        <div>Burger </div>
+        <div>Build Controls </div>
+      </Aux2>
+    );
+  }
+}
+
+export default BurgerBuilder;
