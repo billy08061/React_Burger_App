d3e2af137712a78d97834c2de76f6d74875896cd
William Mitchell <billy.mitchell@levvel.com>
2018-07-25 12:16:17 -0400

    The order summary will now close if clicked on the backdrop, end of video 16

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,18 +1,21 @@
-import React from 'react'; 
-import classes from './Modal.css'; 
+import React from "react";
+import classes from "./Modal.css";
+import Aux2 from "../../../hoc/Aux2";
+import Backdrop from "../Backdrop/Backdrop";
 
-const Modal = (props) => (
-
-    <div className = {classes.Modal}
-    style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1': '0'
-    }}> 
-        {props.children}
+const Modal = props => (
+  <Aux2>
+    <Backdrop show = {props.show} clicked ={props.modalClosed}/>
+    <div
+      className={classes.Modal}
+      style={{
+        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+        opacity: props.show ? "1" : "0"
+      }}
+    >
+      {props.children}
     </div>
-
+  </Aux2>
 );
-    
 
- 
 export default Modal;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -78,6 +78,11 @@ class BurgerBuilder extends Component {
         this.setState({purchasing:true}); 
     }
 
+    purchaseCancelHandler = () => {
+
+            this.setState({purchasing: false}); 
+    }
+
     render () {
         const disabledInfo = {
             ...this.state.ingredients
@@ -88,7 +93,7 @@ class BurgerBuilder extends Component {
         // {salad: true, meat: false, ...}
         return (
             <Aux2>
-                <Modal show={this.state.purchasing}>
+                <Modal show={this.state.purchasing} modalClosed ={this.purchaseCancelHandler}>
                     <OrderSummary ingredients = {this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
