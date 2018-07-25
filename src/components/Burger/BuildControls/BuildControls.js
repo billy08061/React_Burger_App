20876d7483aa075b71256aeff19c7b40bb5ba9b4
William Mitchell <billy.mitchell@levvel.com>
2018-07-25 09:40:40 -0400

    Started the process of implementing build controls to the burger.  Added two buttons that will add and remove things from the burger, end of video 8

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import classes from './BuildControls.css'; 
+
+const BuildControls = (props) => (
+    
+    <div classes ={classes.BuildControls}> 
+        
+
+    </div> 
+); 
+ 
+export default BuildControls;
