69f598d82c6b2e7c466be1b4d00235d45257904a
William Mitchell <billy.mitchell@levvel.com>
2018-07-25 10:21:44 -0400

    buttons now have functionality and styling is different, end of videos 10,11,12,13

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,15 @@
-import React from "react";
+import React from 'react';
 
-import Aux from "../../hoc/Aux2";
-import classes from "./Layout.css"
+import Aux from '../../hoc/Aux2';
+import classes from './Layout.css';
 
-const Layout = props => {
-  return (
+const layout = ( props ) => (
     <Aux>
-      <div> Toolbar, SideDrawer, Backdrop</div>
-      <main className = {classes.Content}>
-         {props.children}
-      </main>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
     </Aux>
-  );
-};
+);
 
-export default Layout;
+export default layout;
