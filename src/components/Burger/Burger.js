12da4af8739ff7638824472674468896b397974a
William Mitchell <billy.mitchell@levvel.com>
2018-07-25 09:09:54 -0400

    Established a dynamic way to outpug ingredients to the burger, end of video 6 :)

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,19 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 
 const Burger = (props) => {
+   
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((__, i) => {
+                return <BurgerIngredient key = {igKey + i} type = {igKey} />
+            }); // [] with two elements
+        }); 
+   
     return (   
         <div className={classes.Burger}> 
         
         <BurgerIngredient type="bread-top" />
-        <BurgerIngredient type="cheese" />
-        <BurgerIngredient type="meat" />
+            {transformedIngredients}
         <BurgerIngredient type="bread-bottom" />
         </div>
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 import Aux2 from "../../hoc/Aux2";
-import Burger from '../../components/Burger/Burger';
-
+import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
+
+  state = {
+      ingredients: {
+          salad: 1, 
+          bacon: 1,
+          cheese: 2, 
+          meat: 2 
+      }
+
+
+
+  };
+
   render() {
     return (
       <Aux2>
-        <div><Burger /> </div>
+        <div>
+          <Burger ingredients ={this.state.ingredients} />{" "}
+        </div>
         <div>Build Controls </div>
       </Aux2>
     );
