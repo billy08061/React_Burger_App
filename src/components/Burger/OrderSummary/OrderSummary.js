4220bdc042670c96abf7a56f77a6865fcb66c383
William Mitchell <billy.mitchell@levvel.com>
2018-07-25 15:39:29 -0400

    Lifecycle methods added for optimization, Application complete

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,40 @@
-import React from 'react';
+import React, { Component } from "react";
 
-import Aux2 from '../../../hoc/Aux2';
-import Button from '../../UI/Button/Button';
+import Aux2 from "../../../hoc/Aux2";
+import Button from "../../UI/Button/Button";
 
-const orderSummary = ( props ) => {
-    const ingredientSummary = Object.keys( props.ingredients )
-        .map( igKey => {
-            return (
-                <li key={igKey}>
-                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
-                </li> );
-        } );
+class OrderSummary extends Component {
+  componentWillUpdate() {
+      console.log('Summary will update'); 
+  }
+  
+    render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+      return (
+        <li key={igKey}>
+          <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+          {this.props.ingredients[igKey]}
+        </li>
+      );
+    });
 
     return (
-        <Aux2>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux2>
+      <Aux2>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Aux2>
     );
-};
-
-export default orderSummary;
+  }
+}
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from './Modal.css';
 import Aux2 from '../../../hoc/Aux2';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = ( props ) => (
-    <Aux2>
-        <Backdrop show={props.show} clicked={props.modalClosed} />
-        <div
-            className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}>
-            {props.children}
-        </div>
-    </Aux2>
-);
+class Modal extends Component{
+    
+    shouldComponentUpdate (nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+            
+        }
+    componentWillUpdate() {
+        console.log('[Modal] WillUpdate'); 
+    }
+    
+    render () {
 
-export default modal;
+        return (
+
+            <Aux2>
+            <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+            <div
+                className={classes.Modal}
+                style={{
+                    transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: this.props.show ? '1' : '0'
+                }}>
+                {this.props.children}
+            </div>
+        </Aux2>
+        )
+
+
+    }
+     
+        
+    
+
+
+
+} 
+
+
+export default Modal;
